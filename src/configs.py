c0c17447913ebadf0ba1459caed20098c978a84e
Egor <yegor.skuratov@gmail.com>
2025-04-08 10:01:00 +0300

    Add files via upload

---
+++ src/configs.py
@@ -0,0 +1,10 @@
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 675
+TOPLEVEL_WINDOW_WIDTH = 600
+TOPLEVEL_WINDOW_HEIGHT = 400
+APPEARANCE_MODE = "dark"
+TITLE = "Flight Management System"
+
+background_image = "assets\\bg_gradient.jpg"
+logo_image = "assets\\logo_picture.png"
+database_path = "application_database.db"
